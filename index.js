40b8b48abe34a71f564480dc71a745ae9a1f1c20
Saidakhror-Kh <xojakbarovsaidaxror@gmail.com>
2021-12-30 02:08:10 +0500

    Add ssh key

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* sasasasa */
+
 const TelegramApi = require('node-telegram-bot-api')
 const { options } = require('nodemon/lib/config')
 const token = '5026585404:AAGnE9wniFFs5XKo4vq-oLm9D8HayhtohNk'
